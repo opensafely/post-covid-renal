3a910caa41b63176aa81e5668386ba5f9885f627
SRW612 <84728159+SRW612@users.noreply.github.com>
2023-06-15 10:40:07 +0100

    add codelist.py file

--- analysis/common_variables.py
+++ analysis/common_variables.py
@@ -493,6 +493,29 @@ def generate_common_variables(index_date_variable,exposure_end_date_variable,out
     # Renal Outcomes
     #----------------------------------------------------------------------------------------
     
+    ## Creatinine measurements to be used for defining AKI and ESRD
+    tmp_out_num_creatinine=patients.mean_recorded_value(
+            creatinine_codes,
+            on_most_recent_day_of_measurement=True,
+            on_or_before = f"{index_date_variable}- 1 day",
+            return_expectations={
+                "float": {"distribution": "normal", "mean": 80, "stddev": 40},
+                "incidence": 0.60,
+            }
+        ),
+
+    ## Acute kidney injury hospital admissions
+    tmp_out_date_aki_hes = patients.admitted_to_hospital(
+        with_these_diagnoses= aki_icd10,
+        returning='date_admitted',
+        between=[f"{index_date_variable}",f"{outcome_end_date_variable}"],
+        date_format="YYYY-MM-DD",
+        return_expectations={
+            "date": {"earliest": study_dates["pandemic_start"], "latest" : "today"},
+            "rate": "uniform",
+            "incidence": 0.1,
+        },
+
     ## IBS as an example of constructing an outcome
     tmp_out_date_ibs_snomed = patients.with_these_clinical_events(
         ibs_snomed,
