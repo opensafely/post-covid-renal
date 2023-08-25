9014f931f7b27d0f42e78e5c3fb6b02e475c7e23
SRW612 <84728159+SRW612@users.noreply.github.com>
2023-08-25 06:36:51 +0100

    Rerun active_analyses

--- analysis/active_analyses.R
+++ analysis/active_analyses.R
@@ -42,7 +42,18 @@
     episode_event_threshold <- 5L
     covariate_threshold <- 5L
 
-
+    ##Dates
+    
+    study_dates <- fromJSON("output/study_dates.json")
+    
+    prevax_start <- "2020-01-01"
+    prevax_stop<- "2021-12-14"
+    vax_unvax_start<-"2021-06-01"
+    vax_unvax_stop <-"2021-12-14"
+    ##Cut points 
+    prevax_cuts <- "28;197;365;714"
+    vax_unvax_cuts <- "28;197"
+    
     # Specify cohorts --------------------------------------------------------------
 
     cohorts <- c("vax","unvax","prevax")
@@ -55,10 +66,7 @@
                          "out_date_esrd")
 
 
-    all_covars <- c("cov_cat_ethnicity;cov_cat_deprivation;cov_cat_smoking_status;cov_bin_carehome_status;",
-                    "cov_num_consulation_rate;cov_bin_healthcare_worker;cov_bin_dementia;cov_bin_liver_disease;",
-                    "cov_bin_chronic_kidney_disease;cov_bin_cancer;cov_bin_hypertension;cov_bin_diabetes;",
-                    "cov_bin_obesity;cov_bin_chronic_obstructive_pulmonary_disease;cov_bin_ami;cov_bin_stroke_isch")
+    all_covars <- c("cov_cat_ethnicity;cov_cat_deprivation;cov_cat_smoking_status;cov_bin_carehome_status;cov_num_consulation_rate;cov_bin_healthcare_worker;cov_bin_dementia;cov_bin_liver_disease;cov_bin_chronic_kidney_disease;cov_bin_cancer;cov_bin_hypertension;cov_bin_diabetes;cov_bin_obesity;cov_bin_chronic_obstructive_pulmonary_disease;cov_bin_ami;cov_bin_stroke_isch")
     
     
     # Add active analyses ----------------------------------------------------------
@@ -132,7 +140,7 @@
                             episode_event_threshold = episode_event_threshold,
                             covariate_threshold = covariate_threshold,
                             age_spline = TRUE,
-                            analysis = "sub_covid_nonhospitalised",
+                            analysis = "sub_covid_nonhospitalised")
         
         ## analysis: sub_covid_history ---------------------------------------------
         
