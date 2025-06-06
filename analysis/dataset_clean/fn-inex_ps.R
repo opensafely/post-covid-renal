428cb5d52ce463aee07bc24e463dc2e91ee30d8e
Emily Eyles <emily.eyles@bristol.ac.uk>
2025-06-06 15:10:52 +0100

    Update project specific exclusion function
    Updated project specific exclusion function to add a second subset, so that the count for the flow diagram would be correct (we want to count how many we excluded (esrd - TRUE), but include the right group (esrd - FALSE)). This is why an extra line is added creating 'input_flow'

--- analysis/dataset_clean/fn-inex_ps.R
+++ analysis/dataset_clean/fn-inex_ps.R
@@ -9,8 +9,9 @@ inex_ps <- function(
   ## Apply inclusion criteria to all cohorts --------------------------------------
   print('Apply project-specific inclusion criteria to all cohorts')
   
+  input_flow <- subset(input, inex_ever_esrd == TRUE) # Subset input if ever had ESRD 
   input <- subset(input, inex_ever_esrd == FALSE) # Subset input if never had ESRD
-  flow[nrow(flow) + 1, ] <- c("Exclusion criteria: Has ESRD prior to index date", nrow(input))
+  flow[nrow(flow) + 1, ] <- c("Exclusion criteria: Has ESRD prior to index date", nrow(input_flow)
   print(flow[nrow(flow), ])
   
   return(list(input = input, flow = flow))
