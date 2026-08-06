b21e4296ace87f56e163d43f0952c380a4d91d2c
Emily Eyles <emily.eyles@bristol.ac.uk>
2026-08-06 14:17:03 +0100

    add unconverged models code, update create project actions
    Added unconverged models code from post-release branch of neuro to come up with a model list for running in Stata
    
    Added the model list to the create-project-actions file

--- analysis/create_project_actions.R
+++ analysis/create_project_actions.R
@@ -41,12 +41,47 @@ age_str <- paste0(
 describe <- FALSE # This prints descriptive files for each dataset in the pipeline
 
 # List of models excluded from model output generation
-# Test first to see which models will be excluded
 
-excluded_models <- c(
-  "cohort_vax-main_preex_FALSE-pneumonia",
-  "cohort_prevax-sub_age_18_39_preex_TRUE-pf"
+#excluded_models <- c()
+
+# List of models that should run in Stata due to convergence issue
+
+stata_models <- unique(c(
+  active_analyses$name[
+    grepl("aki", active_analyses$name) 
+  ],
+  active_analyses$name[
+    grepl("sub_covidhistory", active_analyses$name) 
+  ],
+  active_analyses$name[
+    grepl("sub_covidhospital_TRUE", active_analyses$name) &
+      active_analyses$name !=
+      "cohort_vax-sub_covidhospital_TRUE_preex_FALSE-ckd"
+  ],
+    #selected by code
+    "cohort_unvax-main_preex_FALSE-ckd",
+    "cohort_prevax-sub_age_18_39_preex_FALSE-ckd",
+    "cohort_prevax-sub_age_18_39_preex_TRUE-esrd",
+    "cohort_prevax-sub_age_40_59_preex_TRUE-esrd",
+    "cohort_unvax-sub_age_80_110_preex_FALSE-ckd",
+    "cohort_vax-sub_age_40_59_preex_TRUE-esrd",
+    "cohort_unvax-sub_covidhospital_FALSE_preex_FALSE-ckd",
+    "cohort_prevax-sub_ethnicity_black_preex_FALSE-ckd",
+    "cohort_prevax-sub_ethnicity_black_preex_TRUE-esrd",
+    "cohort_prevax-sub_ethnicity_mixed_preex_FALSE-ckd",
+    "cohort_unvax-sub_ethnicity_asian_preex_FALSE-ckd",
+    "cohort_unvax-sub_ethnicity_black_preex_FALSE-ckd",
+    "cohort_vax-sub_ethnicity_black_preex_FALSE-ckd",
+    "cohort_vax-sub_ethnicity_mixed_preex_FALSE-ckd",
+    "cohort_unvax-sub_sex_male_preex_FALSE-ckd",
+    #selected by examination of outputs
+    "cohort_unvax-main_preex_TRUE-esrd",
+    "cohort_unvax-sub_age_18_39_preex_FALSE-ckd",
+    "cohort_unvax-sub_age_40_59_preex_FALSE-ckd",
+    "cohort_prevax-sub_ethnicity_white_preex_FALSE-ckd"
+  )
 )
+
 
 # Create generic action function -----------------------------------------------
 
---
+++ analysis/find_unconverged_models.R
@@ -0,0 +1,78 @@
+# find_unconverged_models.R - a script to identify models that have not converged based on their hazard ratios and confidence intervals.
+# from neurodegenerative repository: https://github.com/opensafely/post-covid-neurodegenerative/blob/post-release/analysis/post_release/find_unconverged_models.R
+
+# Load libraries ---------------------------------------------------------------
+print('Load libraries')
+
+library(magrittr)
+library(tidyverse)
+library(purrr)
+library(data.table)
+library(tidyverse)
+library(svglite)
+library(VennDiagram)
+library(grid)
+library(gridExtra)
+
+# Specify paths ----------------------------------------------------------------
+print('Specify paths')
+
+# NOTE:
+# This file is used to specify paths and is in the .gitignore to keep your information secret.
+# A file called specify_paths_example.R is provided for you to fill in.
+# Please remove "_example" from the file name and add your specific file paths before running this script.
+
+source("analysis/specify_paths.R")
+
+# Source functions -------------------------------------------------------------
+print('Source functions')
+
+source("analysis/utility.R")
+
+# Make post-release directory --------------------------------------------------
+print('Make post-release directory')
+
+dir.create("output/post_release/", recursive = TRUE, showWarnings = FALSE)
+output_folder <- "output/post_release/"
+
+# Find all model files ---------------------------------------------------------
+print('Collecting list of models')
+file_list <- list.files(
+  path = path_release,
+  pattern = "^model_output-.*-midpoint6\\.csv$",
+  full.names = TRUE
+)
+
+# Read and combine all CSV files into one data frame---------------------------
+print('Loading and combining models')
+df <- file_list %>%
+  lapply(read_csv, show_col_types = FALSE) %>%
+  bind_rows()
+
+# Define bounds for checking --------------------------------------------------
+lb <- 0.25
+ub <- 32
+
+# Find relevant rows ----------------------------------------------------------
+print('Filtering dataframe')
+df <- df[df$hr < lb | df$hr > ub | df$conf_low < lb | df$conf_high > ub, ] # HR range restriction
+df <- df[!(df$term %in% c("days_pre", "days0_1")), ] # Term restriction
+df <- df[!is.na(df$name), ] #NA filtering
+
+unconverged_list <- sprintf('"%s"', unique(df$name))
+
+# Save list of models ---------------------------------------------------------
+print("Saving .txt list of models")
+writeLines(
+  paste(unconverged_list, collapse = ",\n"),
+  "output/post_release/unconverged_models.txt"
+)
+
+# Save related dataframe -------------------------------------------------------
+print("Save dataframe of unconverged models")
+
+readr::write_csv(
+  df,
+  paste0(output_folder, "/unconverged_models.csv"),
+  na = "-"
+)
